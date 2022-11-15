073483aa1163d2bdb8a249c1ba69a13ed045fdb9
Manikandan <manikandan.arjunan@cloudsense.com>
2022-11-15 11:11:37 +0530

    crystal balls algo edge case handled

--- __tests__/twocrystalballs.test.ts
+++ __tests__/twocrystalballs.test.ts
@@ -1,25 +1,26 @@
-import { twoCrystalBalls } from '../algos/twocrystalballs';
+import { twoCrystalBalls } from "../algos/twocrystalballs";
 
-test('two crystal balls', function () {
-	let idx = Math.floor(Math.random() * 10000);
-	const data = new Array(10000).fill(false);
+test("two crystal balls", function () {
+  let idx = Math.floor(Math.random() * 10000);
+  const data = new Array(10000).fill(false);
 
-	for (let i = idx; i < 10000; ++i) {
-		data[i] = true;
-	}
+  for (let i = idx; i < 10000; ++i) {
+    data[i] = true;
+  }
 
-	expect(twoCrystalBalls(data)).toEqual(idx);
-	expect(twoCrystalBalls(new Array(821).fill(false))).toEqual(-1);
+  expect(twoCrystalBalls(data)).toEqual(idx);
+  expect(twoCrystalBalls(new Array(821).fill(false))).toEqual(-1);
 });
 
-test('two crystal balls test 2g', () => {
-	let idx = Math.floor(Math.random() * 100);
-	const data = new Array(100).fill(false);
+test("two crystal balls test 2g", () => {
+  let idx = Math.floor(Math.random() * 100);
+  const data = new Array(100).fill(false);
 
-	for (let i = idx; i < 100; ++i) {
-		data[i] = true;
-	}
+  // for (let i = idx; i < 100; ++i) {
+  // 	data[i] = true;
+  // }
 
-	expect(twoCrystalBalls(data)).toEqual(idx);
-	expect(twoCrystalBalls(new Array(821).fill(false))).toEqual(-1);
+  data[99] = true
+  expect(twoCrystalBalls(data)).toEqual(99);
+  expect(twoCrystalBalls(new Array(821).fill(false))).toEqual(-1);
 });
--- algos/twocrystalballs.ts
+++ algos/twocrystalballs.ts
@@ -1,7 +1,7 @@
 export function twoCrystalBalls(arr: Array<boolean>): number {
 	const sqrtJump = Math.floor(Math.sqrt(arr.length));
 
-	for (let i = sqrtJump; i < arr.length; i += sqrtJump) {
+	for (let i = sqrtJump - 1; i < arr.length; i += sqrtJump) {
 		if (arr[i]) {
 			let j = 0;
 			i -= sqrtJump;
@@ -13,7 +13,8 @@ export function twoCrystalBalls(arr: Array<boolean>): number {
 				j++;
 				i++;
 			}
-			return -1;
+
+			return i;
 		}
 	}
 
